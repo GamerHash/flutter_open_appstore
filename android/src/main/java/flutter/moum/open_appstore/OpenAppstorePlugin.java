3e52be7da5e6cca8a65d874e45638fddd062e0b1
Aron Onak <arononak@gmail.com>
2020-12-22 13:26:43 +0100

    Added support for HUAWEI AppGallery

--- android/src/main/java/flutter/moum/open_appstore/OpenAppstorePlugin.java
+++ android/src/main/java/flutter/moum/open_appstore/OpenAppstorePlugin.java
@@ -3,7 +3,8 @@ package flutter.moum.open_appstore;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-
+import android.widget.Toast;
+import android.os.Build;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -21,42 +22,56 @@ public class OpenAppstorePlugin implements FlutterPlugin {
   private MethodChannel channel;
 
   public static void registerWith(Registrar registrar) {
-
     OpenAppstorePlugin plugin = new OpenAppstorePlugin();
     plugin.setMethodChannel(registrar.context(), registrar.messenger());
   }
+  
+  public static String getDeviceName() {
+    String manufacturer = Build.MANUFACTURER;
+    String model = Build.MODEL;
+    if (model.startsWith(manufacturer)) {
+        return model;
+    }
+    return manufacturer + " " + model;
+  }
+  
+  public static void launchActivity(Context context, String uri) {
+    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    context.startActivity(intent);
+  }
 
   private void setMethodChannel(final Context context, BinaryMessenger messenger) {
-
     channel = new MethodChannel(messenger, "flutter.moum.open_appstore");
     channel.setMethodCallHandler(new MethodCallHandler() {
       @Override
       public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
         if (call.method.equals("getPlatformVersion")) {
           result.success("Android " + android.os.Build.VERSION.RELEASE);
-        }
-        else if (call.method.equals("openappstore")) {
+        } else if (call.method.equals("openappstore")) {
           result.success("Android " + android.os.Build.VERSION.RELEASE);
           String android_id = call.argument("android_id");
+          String huawei_id = call.argument("huawei_id");
           String manufacturer = android.os.Build.MANUFACTURER;
+          Toast.makeText(context, getDeviceName(), Toast.LENGTH_SHORT).show();
           if (manufacturer.equals("Amazon")) {
-            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("amzn://apps/android?p=" + android_id)));
+            launchActivity(context, "amzn://apps/android?p=" + android_id);
+          } else if (manufacturer.equalsIgnoreCase("Huawei")) {
+            launchActivity(context, "appmarket://details?id=" + huawei_id);
           } else {
             try {
-              context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + android_id)));
+              launchActivity(context, "market://details?id=" + android_id);
             } catch (android.content.ActivityNotFoundException e) {
-              context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + android_id)));
+              launchActivity(context, "https://play.google.com/store/apps/details?id=" + android_id);
             }
           }
           result.success(null);
-        }
-        else {
+        } else {
           result.notImplemented();
         }
       }
     });
   }
-
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
--- lib/open_appstore.dart
+++ lib/open_appstore.dart
@@ -3,16 +3,14 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class OpenAppstore {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter.moum.open_appstore');
+  static const MethodChannel _channel = const MethodChannel('flutter.moum.open_appstore');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static void launch({String androidAppId, String iOSAppId}) async {
-    await _channel.invokeMethod(
-        'openappstore', {'android_id': androidAppId, 'ios_id':iOSAppId});
+  static void launch({String androidAppId, String huaweiAppId, String iOSAppId}) async {
+    await _channel.invokeMethod('openappstore', {'android_id': androidAppId, 'huawei_id': huaweiAppId, 'ios_id':iOSAppId});
   }
 }
