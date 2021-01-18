e3f110707851b9acbc027b8cc8d709cc8a09c5e7
aronappverk <73943827+aronappverk@users.noreply.github.com>
2021-01-18 13:44:13 +0100

    Update open_appstore.dart
    Added storeType

--- lib/open_appstore.dart
+++ lib/open_appstore.dart
@@ -4,10 +4,27 @@ import 'package:flutter/services.dart';
 
 class OpenAppstore {
   static const MethodChannel _channel = const MethodChannel('flutter.moum.open_appstore');
+  
+  enum StoreType { GooglePlay, AppGallery, AppStore, Nothing }
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+  
+  static Future<StoreType> get storeType async {
+    if (Platform.isAndroid) {
+      final String type = await _channel.invokeMethod('getStoreType');
+      if (type == "google") {
+        return StoreType.GooglePlay;
+      } else if (type == "huawei") {
+        return StoreType.AppGallery;
+      }
+    } else if (Platform.isIOS) {
+      return StoreType.AppStore;
+    } else {
+      return StoreType.Nothing;
+    }
   }
 
   static void launch({String androidAppId, String huaweiAppId, String iOSAppId}) async {
